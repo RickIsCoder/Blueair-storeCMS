36540979ba02b089f9af986c84ad293ddd36d3cb
Rick li <285699093@qq.com>
2016-03-10 21:03:21 +0800

    change homepage

--- application/views/frontEnd/js/action.js
+++ application/views/frontEnd/js/action.js
@@ -1,10 +1,10 @@
-$(function(){
-    
+$(function () {
+
     var winWidth = $(window).width();
     var winHeight = $(window).height();
-    
+
     $('.slickContainer').height(winHeight).width(winWidth);
-   
+
     $('#whyBlueAirSlide').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -12,109 +12,141 @@ $(function(){
         dots: true,
         infinite: false,
     });
-    
-    $('#navBtn').on('click', function(){
-        $('#nav').addClass('actived');
-        $("#navMask").addClass('actived');
+
+    // nav
+    $('#navBtn').on('click', function (event) {
+        var e = $(event.currentTarget);
+        switch (e.data("state")) {
+        case "efficient":
+            whyBAShow();
+            $('#efficient').removeClass("inShow");
+            resetNavBtn();
+            break;
+        default:
+            $('#nav').addClass('actived');
+            $("#navCloseBtn").addClass('actived');
+        }
     });
-    
-    $('#navMask').on('click', function(){
+
+    $('#navCloseBtn').on('click', function () {
         $('#nav').removeClass('actived');
-        $('#navMask').removeClass('actived');
+        $('#navCloseBtn').removeClass('actived');
     });
+    // end nav
+
+    // whyBlueAir
+    function whyBADismiss() {
+        $("#whyBlueAirContainer").addClass("dismiss");
+    }
+
+    function whyBAShow() {
+        $("#whyBlueAirContainer").removeClass("dismiss");
+    }
+
+    function changeNavBtn() {
+        $("#navBtn").addClass("downArrow").data("state", "efficient");
+    }
     
-    $('#honorIdenty').on('click', function(event){
+    function resetNavBtn() {
+        $("#navBtn").removeClass("downArrow").data("state", "normal");
+    }
+
+    $('#jumpEfficient').on('click', function (event) {
+        whyBADismiss();
+        changeNavBtn();
+        $('#efficient').addClass("inShow");
+    });
+
+    $('#honorIdenty').on('click', function (event) {
         $('.honorContentContainer').removeClass('actived');
         $('#honorIdentyContent').addClass('actived');
         $('.honorNav').removeClass('actived');
         $(event.currentTarget).addClass('actived');
     });
-    
-    $('#honorReport').on('click', function(event){
+
+    $('#honorReport').on('click', function (event) {
         $('.honorContentContainer').removeClass('actived');
         $('#honorReportContent').addClass('actived');
         $('.honorNav').removeClass('actived');
         $(event.currentTarget).addClass('actived');
     });
-    
-    $('#jumpEfficient').on('click', function(event){
-        $('#whyBlueAirContainer').removeClass("inShow fadeIn").addClass("fadeOut");
-        $('#efficient').addClass("fadeIn inShow").removeClass("fadeOut");
-        $('#efficientDark').removeClass("fadeOut").addClass('actived'); 
-    });
-    
-    $('#efficientBack').on('click', function(event){
+
+
+
+    $('#efficientBack').on('click', function (event) {
         $('#whyBlueAirContainer').addClass("inShow fadeIn").removeClass("fadeOut");
         $('#efficient').addClass("fadeOut").removeClass("inShow");
-        $('#efficientDark').removeClass('actievd'); 
+        $('#efficientDark').removeClass('actievd');
         $('#efficientLight').removeClass('actived fadeOut');
     });
-    
-    $('#jumpMute').on('click', function(){
+
+    $('#jumpMute').on('click', function () {
         $('#whyBlueAirContainer').removeClass("inShow fadeIn").addClass("fadeOut");
         $('#mute').addClass("fadeIn inShow").removeClass("fadeOut");
     });
-    
-    $('#muteBack').on('click', function(){
+
+    $('#muteBack').on('click', function () {
         $('#whyBlueAirContainer').addClass("inShow fadeIn").removeClass("fadeOut");
         $('#mute').addClass("fadeOut").removeClass("inShow");
     });
-    
-    $('#jumpEnergy').on('click', function(){
+
+    $('#jumpEnergy').on('click', function () {
         $('#whyBlueAirContainer').removeClass("inShow fadeIn").addClass("fadeOut");
         $('#energy').addClass("fadeIn inShow").removeClass("fadeOut");
     });
-    
-    $('#energyBack').on('click', function(){
+
+    $('#energyBack').on('click', function () {
         $('#whyBlueAirContainer').addClass("inShow fadeIn").removeClass("fadeOut");
         $('#energy').addClass("fadeOut").removeClass("inShow");
     });
-    
-    $('#jumpByproduct').on('click', function(){
+
+    $('#jumpByproduct').on('click', function () {
         $('#whyBlueAirContainer').removeClass("inShow fadeIn").addClass("fadeOut");
         $('#byproduct').addClass("fadeIn inShow").removeClass("fadeOut");
     });
-    
-    $('#byproductBack').on('click', function(){
+
+    $('#byproductBack').on('click', function () {
         $('#whyBlueAirContainer').addClass("inShow fadeIn").removeClass("fadeOut");
         $('#byproduct').addClass("fadeOut").removeClass("inShow");
     });
-    
-    $('#openBlueAir').on('click', function(event){
-        $('#efficientDark').addClass('fadeOut'); 
+
+    $('#openBlueAir').on('click', function (event) {
+        $('#efficientDark').addClass('fadeOut');
         $('#efficientLight').addClass('fadeIn actived');
     });
-    
-    
-    $('#btnWhyEfficient').on('click', function(event){
+
+
+    $('#btnWhyEfficient').on('click', function (event) {
         $('#efficient').addClass('slideOutLeft');
         $('#whyEfficient').addClass('slideInRight').addClass('inShow');
     });
-    
-    $('#whyEfficientBack').on('click', function(event){
+
+    $('#whyEfficientBack').on('click', function (event) {
         $('#whyEfficient').addClass('slideOutRight').removeClass("slideInRight");
         $('#efficient').addClass('slideInLeft').removeClass('slideOutLeft');
     });
-    
-    $('#energyBtn1').on('click', function(){
+
+    $('#energyBtn1').on('click', function () {
         $('#energy1Img').addClass("fadeIn");
         $('#energy1Content').addClass("fadeIn aniDelay");
     });
-    
-    $('#energyBtn2').on('click', function(){
+
+    $('#energyBtn2').on('click', function () {
         $('#energy2Img').addClass("fadeIn");
         $('#energy2Content').addClass("fadeIn aniDelay");
     });
-    
-    $('#energyBtn3').on('click', function(){
+
+    $('#energyBtn3').on('click', function () {
         $('#energy3Img').addClass("fadeIn");
         $('#energy3Content').addClass("fadeIn aniDelay");
     });
-    
-    $('#energyBtn4').on('click', function(){
+
+    $('#energyBtn4').on('click', function () {
         $('#energy4Img').addClass("fadeIn");
         $('#energy4Content').addClass("fadeIn aniDelay");
         $('#energyHonor').addClass("fadeIn aniDelay");
-    });    
-    
+    });
+
+    // end whyBlueAir
+
 });
